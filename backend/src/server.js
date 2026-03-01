0efdd5d1553880d3ea2e462dfb80443c3161bfd7
Muhammad Saif <saifmuhammad779@gmail.com>
2026-03-01 15:55:36 +0530

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,10 +2,20 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import messageroutes from "./routes/message.route.js";
+import path from "path";
 
 dotenv.config();
 
+
+
+
 const app = express();
+const __dirname = path.resolve();
+
+const PORT = process.env.PORT || 5000;
+
+
+
 
 // Middleware
 app.use(express.json());
@@ -13,8 +23,18 @@ app.use(express.json());
 // Routes
 app.use("/api/auth" , authRoutes);
 app.use("/api/messages" , messageroutes);
+
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
+
+
 // Use PORT from .env
-const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log("Server running on port : " + PORT);
